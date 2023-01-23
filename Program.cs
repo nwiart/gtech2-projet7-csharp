fbbe3a3668ab5f52330731d4073e21aae80fc794
nwiart <nwiart@gaming.tech>
2023-01-23 10:02:06 +0100

    Basic game loop + keyboard input

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
-﻿namespace ConsoleGame
+﻿using System;
+
+namespace ConsoleGame
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello world!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            ConsoleKey lastKey = 0;
+
+            // Game loop.
+            while (true)
+			{
+				// Key input.
+                while (Console.KeyAvailable)
+				{
+					lastKey = Console.ReadKey().Key;
+					if (lastKey == ConsoleKey.A)
+					{
+						return;
+					}
+				}
+
+				// Render.
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(0, 0);
+				for (int i = 0; i < Console.WindowWidth; ++i)
+				{
+					Console.Write("#");
+				}
+
+                Console.SetCursorPosition(10, 10);
+				Console.Write("You pressed " + lastKey.ToString() + "!");
+
+                Console.SetCursorPosition(0, Console.WindowHeight - 1);
+				for (int i = 0; i < Console.WindowWidth - 1; ++i)
+				{
+					Console.Write("#");
+				}
+			}
+		}
+	}
 }
