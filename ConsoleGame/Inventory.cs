c2f2018a7ed84525e11dc6a4455b105930266fb2
Valentin Faguet <valentin@wallmatt.com>
2023-01-31 13:58:56 +0100

    Inventory && Items starting

--- ConsoleGame/Inventory.cs
+++ ConsoleGame/Inventory.cs
@@ -2,6 +2,38 @@ namespace ConsoleGame
 {
     public class Inventory
     {
-        
+        public class InventorySlot
+        {
+            public Item? Item { get; set; }
+            public int Amount { get; set; }
+        }
+    }
+
+
+    public class Item
+    {
+        // Type of Items
+        public enum ItemTypes
+        {
+            Weapon,
+            Armor,
+            Consumable,
+            QuestItem,
+            Misc
+        }
+        public string? Name { get; set; }
+        public int Value { get; set; }
+        public int Weight { get; set; }
+        public int Damage { get; set; }
+        public int Defense { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+        public int Speed { get; set; }
+
+        // Make it specific for armor and weapons
+        public int AttackSpeed { get; set; }
+        public int AttackRange { get; set; }
+        public int AttackCooldown { get; set; }
+        public int AttackDamage { get; set; }
     }
 }
