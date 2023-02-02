696622a6cca8c6888b987aaf04bf7a1b828dbb05
Valentin Faguet <valentin@wallmatt.com>
2023-02-02 16:15:20 +0100

    Fix Evolve function

--- ConsoleGame/Inventory/BeastItem.cs
+++ ConsoleGame/Inventory/BeastItem.cs
@@ -57,10 +57,11 @@ namespace ConsoleGame.Inventory
         public void GainExperience(int exp)
         {
             Experience += exp;
+            CheckEvolve();
         }
-        public void CheckEvolve(Beast.Beast b)
+        public void CheckEvolve()
         {
-            if (Experience >= b.ExperienceToEvolve)
+            if (Experience >= Beast.ExperienceToEvolve)
             {
                 Level++;
                 Attack += (int)ValuesPerLevel.Attack;
@@ -68,7 +69,7 @@ namespace ConsoleGame.Inventory
                 Mana += (int)ValuesPerLevel.Mana;
                 Defense += (int)ValuesPerLevel.Defense;
 
-                Experience -= b.ExperienceToEvolve;
+                Experience -= Beast.ExperienceToEvolve;
 
             }
         }
